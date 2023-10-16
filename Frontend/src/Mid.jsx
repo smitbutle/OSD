72f9d75581e8a63954a2e8f96385c931f7f5de35
Smit Butle <smit2003.sb@gmail.com>
2023-10-17 01:56:46 +0530

    UI changes

--- Frontend/src/Mid.jsx
+++ Frontend/src/Mid.jsx
@@ -3,10 +3,18 @@ import styles from './Mid.module.css'
 const Mid = () => {
   return (
     <div className={styles.bg}>
-      <img src="https://i.ibb.co/sFCSgHc/OSD-1-1.png" alt="bg" />
+      {/* <img src="https://i.ibb.co/sFCSgHc/OSD-1-1.png" alt="bg" /> */}
+      {/* <img src="./assets/OSD.svg" alt="bg" /> */}
 
+      <img src="https://i.ibb.co/JtPvvQZ/Copy-of-UI-OSD-2-min.png" alt="Copy-of-UI-OSD-2-min" border="0"></img>
       <div className={styles.watch}>
         <h1>Watch Trailer</h1>
+      </div>
+      <div className={styles.reaper}>
+        <img src="https://i.ibb.co/VC3cpty/reaper.png" alt="reaper" border="0"></img>
+        <div className={styles.text}>
+          Dummy text! Open Source Day is a celebration of open source where Walchand Linux Users' Group conducts a free-of-cost workshop. Walchand Linux Users' Group conducts a free-of-cost workshop. Open Source Day is a celebration of open-source where Walchand Linux Users' Group conducts a free of cost workshop.
+        </div>
       </div>
       <div className={styles.vid}>
         {/* <h2>If you have paid the amount and didn't recieve confirma email,<br /> please contact Datta Gangji +91 75177 56075</h2> */}
@@ -14,7 +22,7 @@ const Mid = () => {
 
         <video controls >
 
-          <source src="../public/LD.mp4" type="video/mp4" />
+          <source src="./LD.mp4" type="video/mp4" />
         </video>
       </div>
     </div>
